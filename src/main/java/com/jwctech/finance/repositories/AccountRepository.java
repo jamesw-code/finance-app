a149d1f7f7c1e0d64955ca2586f2be5d081ffd3f
James Wyatt <jamesrwyatt35@gmail.com>
2025-10-27 14:48:28 -0400

    add annotation

--- src/main/java/com/jwctech/finance/repositories/AccountRepository.java
+++ src/main/java/com/jwctech/finance/repositories/AccountRepository.java
@@ -2,9 +2,11 @@ package com.jwctech.finance.repositories;
 
 import com.jwctech.finance.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByBusinessIdOrderByNameAsc(Long businessId);
 
--- src/main/java/com/jwctech/finance/repositories/BusinessRepository.java
+++ src/main/java/com/jwctech/finance/repositories/BusinessRepository.java
@@ -2,7 +2,9 @@ package com.jwctech.finance.repositories;
 
 import com.jwctech.finance.entities.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
     boolean existsByNameIgnoreCase(String name);
 }
