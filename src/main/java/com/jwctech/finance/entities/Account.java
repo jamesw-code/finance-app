d9c75490fa07ae09b9493d14e0718fbb3a551965
James Wyatt <jamesrwyatt35@gmail.com>
2025-10-27 15:00:11 -0400

    update account entity

--- src/main/java/com/jwctech/finance/entities/Account.java
+++ src/main/java/com/jwctech/finance/entities/Account.java
@@ -11,9 +11,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "accounts")
+@Data
 public class Account {
 
     @Id
@@ -30,6 +32,9 @@ public class Account {
     @JoinColumn(name = "business_id", nullable = false)
     @JsonIgnore
     private Business business;
+
+    @Column(name = "business_id", insertable = false, updatable = false)
+    private Long businessId;
 
     public Account() {
     }
