b41f86e525ecc73242ac53dbafcbbb1449c01755
James Wyatt <78618659+jamesw-code@users.noreply.github.com>
2025-10-27 14:49:22 -0400

    feat: add business categories management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {Layout} from './layout/layout';
 import {Dashboard} from './dashboard/dashboard';
 import {Accounts} from './pages/accounts/accounts';
+import {Categories} from './pages/categories/categories';
 import {NotFound} from './pages/not-found/not-found';
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
       { path: 'dashboard', component: Dashboard },
       // { path: 'transactions', component: TransactionsComponent },
       { path: 'accounts', component: Accounts },
+      { path: 'categories', component: Categories },
       // { path: 'reports', component: ReportsComponent },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ]
